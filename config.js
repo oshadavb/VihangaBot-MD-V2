3e233a46e17ba80b480a9caefa395f1ceb910d06
oshadavb <111900344+oshadavb@users.noreply.github.com>
2022-08-23 23:47:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '94761036291' //මෙතන number එක.
 
 // Other
 global.owner = ['94761036291',''] //මෙතන number එක.
